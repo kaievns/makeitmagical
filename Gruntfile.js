dfc977da382695d85616355b5e46e1413ce04a47
Nikolay Nemshilov <nemshilov@gmail.com>
2014-06-10 18:29:28 +1000

    Hooking up grunt to less and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,39 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish'),
+        node: true
+      },
+      files: ['index.js', 'app/*.js', 'config/*.js', 'public/*.js']
+    },
+    less: {
+      compile: {
+        files: {
+          "public/application.css" : "public/application.less"
+        }
+      }
+    },
     watch: {
       options: {
         livereload: true,
       },
       js: {
-        files: ['index.js', 'app/*.js', 'views/*.jade']
+        files: ['index.js', 'app/*.js', 'config/*.js'],
+        tasks: ["jshint"]
       },
-      css: {
-        files: ['public/*.css']
+      less: {
+        files: ['public/*.less'],
+        tasks: ['less:compile'],
+      },
+      jade: {
+        files: ['views/*.jade']
       }
     }
   });
 
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 };
